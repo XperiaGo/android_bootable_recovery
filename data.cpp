9880acaf7b13d5c90f3fa7d46082763f83da9f7f
Agontuk <rifat662@gmail.com>
2014-09-07 00:21:13 +0600

    Fix battery capacity & status path

--- data.cpp
+++ data.cpp
@@ -38,6 +38,10 @@
 #define DEVID_MAX 64
 #define HWID_MAX 32
 
+#ifdef XPERIA_TWRP_TOUCH
+#include "xperia_nxt.h"
+#endif
+
 extern "C"
 {
 	#include "twcommon.h"
@@ -903,7 +907,12 @@ int DataManager::GetMagicValue(const string& varName, string& value)
 			capacity_file += "/capacity";
 			FILE * cap = fopen(capacity_file.c_str(),"rt");
 #else
+	#ifdef XPERIA_TWRP_TOUCH
+			string capacity_file = BATTERY_CAPACITY_FILE;
+			FILE * cap = fopen(capacity_file.c_str(),"rt");
+	#else
 			FILE * cap = fopen("/sys/class/power_supply/battery/capacity","rt");
+	#endif
 #endif
 			if (cap){
 				fgets(cap_s, 4, cap);
@@ -917,7 +926,12 @@ int DataManager::GetMagicValue(const string& varName, string& value)
 			status_file += "/status";
 			cap = fopen(status_file.c_str(),"rt");
 #else
+	#ifdef XPERIA_TWRP_TOUCH
+			string status_file = BATTERY_CHARGING_STATUS_FILE;
+			cap = fopen(status_file.c_str(),"rt");
+	#else
 			cap = fopen("/sys/class/power_supply/battery/status","rt");
+	#endif
 #endif
 			if (cap) {
 				fgets(cap_s, 2, cap);
